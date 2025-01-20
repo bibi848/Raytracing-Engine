ee1929c4fa9b5158fbb755b316f0a5d7e8ffc091
Oscar Djuric <oscar.djuric@gmail.com>
2025-01-20 20:35:26 +1000

    Completion of first book

--- RaytracingEngine/camera.h
+++ RaytracingEngine/camera.h
@@ -17,6 +17,9 @@ public:
     Vec3 lookat = Vec3(0, 0, -1);
     Vec3 vup = Vec3(0, 1, 0);
 
+    double defocus_angle = 0; //Variation angle of rays through each pixel
+    double focus_dist = 10;   // Distance from camera lookfrom point to plane of perfect focus
+
 	void render(const Hittable& world) {
         initialise();
         
@@ -47,6 +50,8 @@ private:
     Vec3   pixel_delta_u;       // Offset to pixel to the right
     Vec3   pixel_delta_v;       // Offset to pixel below
     Vec3   u, v, w;
+    Vec3   defocus_disk_u;
+    Vec3   defocus_disk_v;
 
     void initialise() {
         image_height = int(image_width / aspect_ratio);
@@ -57,10 +62,9 @@ private:
         center = lookfrom;
 
         // Determine viewport dimensions.
-        auto focal_length = (lookfrom - lookat).length();
         auto theta = deg2rad(vfov);
         auto h = std::tan(theta / 2);
-        auto viewport_height = 2 * h * focal_length;
+        auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (double(image_width) / image_height);
 
         // Calculate the u,v,w vectors for the camera coordinate frame.
@@ -77,15 +81,21 @@ private:
         pixel_delta_v = viewport_v / image_height;
 
         // Calculate the location of the upper left pixel.
-        auto viewport_upper_left = center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
+        auto viewport_upper_left = center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        // Calculate the camera defocus disk basis vectors
+        auto defocus_radius = focus_dist * std::tan(deg2rad(defocus_angle / 2));
+        defocus_disk_u = u * defocus_radius;
+        defocus_disk_v = v * defocus_radius;
+
     }
 
     Ray get_ray(int i, int j) const {
 
         auto offset = sample_square();
         auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
-        auto ray_origin = center;
+        auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
 
         return Ray(ray_origin, ray_direction);
@@ -93,6 +103,11 @@ private:
 
     Vec3 sample_square() const {
         return Vec3(random_double() - 0.5, random_double() - 0.5, 0);
+    }
+
+    Vec3 defocus_disk_sample() const {
+        auto p = random_in_unit_disk();
+        return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
     }
 
 
--- RaytracingEngine/main.cpp
+++ RaytracingEngine/main.cpp
@@ -12,29 +12,62 @@
 int main() {
     hittable_list world;
 
-    auto material_ground = make_shared<Lambertian>(colour(0.8, 0.8, 0.0));
-    auto material_center = make_shared<Lambertian>(colour(0.1, 0.2, 0.5));
-    auto material_left = make_shared<Dielectric>(1.50);
-    auto material_bubble = make_shared<Dielectric>(1.00 / 1.50);
-    auto material_right = make_shared<Metal>(colour(0.8, 0.6, 0.2), 0.0);
+    auto ground_material = make_shared<Lambertian>(colour(0.5, 0.5, 0.5));
+    world.add(make_shared<Sphere>(Vec3(0, -1000, 0), 1000, ground_material));
 
-    world.add(make_shared<Sphere>(Vec3(0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<Sphere>(Vec3(0.0, 0.0, -1.2), 0.5, material_center));
-    world.add(make_shared<Sphere>(Vec3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<Sphere>(Vec3(-1.0, 0.0, -1.0), 0.4, material_bubble));
-    world.add(make_shared<Sphere>(Vec3(1.0, 0.0, -1.0), 0.5, material_right));
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            Vec3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+            if ((center - Vec3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<Material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = colour::random() * colour::random();
+                    sphere_material = make_shared<Lambertian>(albedo);
+                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                }
+                else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = colour::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<Metal>(albedo, fuzz);
+                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                }
+                else {
+                    // glass
+                    sphere_material = make_shared<Dielectric>(1.5);
+                    world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<Dielectric>(1.5);
+    world.add(make_shared<Sphere>(Vec3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<Lambertian>(colour(0.4, 0.2, 0.1));
+    world.add(make_shared<Sphere>(Vec3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<Metal>(colour(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<Sphere>(Vec3(4, 1, 0), 1.0, material3));
 
     Camera camera;
 
     camera.aspect_ratio = 16.0 / 9.0;
-    camera.image_width = 800;
-    camera.samples_per_pixel = 100;
+    camera.image_width = 1200;
+    camera.samples_per_pixel = 10;
     camera.max_depth = 50;
 
-    camera.vfov = 30;
-    camera.lookfrom = Vec3(-2, 2, 1);
-    camera.lookat = Vec3(0, 0, -1);
+    camera.vfov = 20;
+    camera.lookfrom = Vec3(13, 2, 3);
+    camera.lookat = Vec3(0, 0, 0);
     camera.vup = Vec3(0, 1, 0);
+
+    camera.defocus_angle = 0.6;
+    camera.focus_dist = 10.0;
 
     camera.render(world);
 
